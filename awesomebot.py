8a1159da23dd133f86b0a67894899242b373c127
alexgrowler <aiaksenov.spb@gmail.com>
2018-12-09 15:38:26 +0300

    efmem

--- awesomebot.py
+++ awesomebot.py
@@ -1,9 +1,9 @@
 import telebot
 
 
-access_token = "696871290:AAHjTGJQwyx6pm4qz5eJinAfxsaP_OefkIU"
 
-bot = telebot.TeleBot(access_token)
+
+bot = telebot.TeleBot("696871290:AAHjTGJQwyx6pm4qz5eJinAfxsaP_OefkIU")
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
